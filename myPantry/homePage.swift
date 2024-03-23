29e040501fc88430520fc017e4571d02c0bd8ca0
Abhinav_p5 <abhinavpappu05@gmail.com>
2024-03-23 15:49:09 -0400

    login page changes

---
+++ myPantry/homePage.swift
@@ -0,0 +1,18 @@
+//
+//  homePage.swift
+//  myPantry
+//
+//  Created by Abhinav Pappu on 3/23/24.
+//
+
+import SwiftUI
+
+struct homePage: View {
+    var body: some View {
+        Text("uhhhh")
+    }
+}
+
+#Preview {
+    homePage()
+}
--- myPantry/loginPage.swift
+++ myPantry/loginPage.swift
@@ -1,57 +1,50 @@
-//
-//  loginPage.swift
-//  myPantry
-//
-//  Created by Abhinav Pappu on 3/23/24.
-//
-
 import SwiftUI
 
+
 struct LoginPage: View {
-    @State private var usernameOrPhone = ""
+    @State private var email = ""
     @State private var password = ""
+    @State private var shouldNavigate = false
+    
+    
     
     var body: some View {
-        VStack {
-            Text("Login")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(.bottom, 20)
+        NavigationStack {
+            VStack {
+                Text("myPantry")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom, 20)
 
-            TextField("Username or Phone Number", text: $usernameOrPhone)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
-
-            SecureField("Password", text: $password)
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
-
-            Button(action: login) {
-                Text("Login")
-                    .font(.headline)
-                    .foregroundColor(.white)
+                TextField("Email", text: $email)
                     .padding()
-                    .frame(width: 220, height: 60)
-                    .background(Color.green)
-                    .cornerRadius(15.0)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 20)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 20)
+
+                Button(action: {
+                    self.shouldNavigate = true
+                }) {
+                    Text("Login")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 220, height: 60)
+                        .background(Color.green)
+                        .cornerRadius(15.0)
+                }
+            }
+            .padding()
+            .navigationDestination(isPresented: $shouldNavigate) {
+                homePage()
             }
         }
-        .padding()
-    }
-
-    
-    
-    
-    func login() {
-        // Perform login action here
-        // Validate usernameOrPhone and password
-        // You might need to differentiate between a phone number and a username
-        // Then proceed with the login logic
-        print("Login button pressed")
     }
 }
 
@@ -60,4 +53,3 @@ struct LoginPage_Previews: PreviewProvider {
         LoginPage()
     }
 }
-
