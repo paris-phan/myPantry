915e799dceef910d3a92de3a9ea9682b8bf0f0c0
Paris <parisphan1234@gmail.com>
2024-03-23 12:03:36 -0400

    init commit

---
+++ myPantry/myPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  myPantryApp.swift
+//  myPantry
+//
+//  Created by Paris Phan on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct myPantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
