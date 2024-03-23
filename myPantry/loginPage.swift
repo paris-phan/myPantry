8b185bf7150e2b07512ada44241ed244ee8caa82
Abhinav_p5 <abhinavpappu05@gmail.com>
2024-03-23 14:06:58 -0400

    init login page

--- myPantry/loginPage.swift
+++ myPantry/loginPage.swift
@@ -1,45 +1,48 @@
-//
-//  loginPage.swift
-//  myPantry
-//
-//  Created by Abhinav Pappu on 3/23/24.
-//
+
 import SwiftUI
 
 struct LoginPage: View {
-    @State private var name: String = ""
-    @State private var phone: String = ""
-    @State private var password: String = ""
-
+    @State private var usernameOrPhone = ""
+    @State private var password = ""
     
     var body: some View {
-        NavigationView {
-            Form {
-                Section(header: Text("Enter your info:")) {
-                    TextField("Name", text: $name)
-                        .autocapitalization(.words)
-                        .disableAutocorrection(true)
-                    
-                    TextField("Phone", text: $phone)
-                        .keyboardType(.emailAddress)
-                        .autocapitalization(.none)
-                        .disableAutocorrection(true)
-                    
-                    SecureField("Password", text: $password)
-                }
+        VStack {
+            Text("Login")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
 
-                Button(action: logIn) {
-                    Text("Log In")
-                }
+            TextField("Username or Phone Number", text: $usernameOrPhone)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+
+            SecureField("Password", text: $password)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+
+            Button(action: login) {
+                Text("Login")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 220, height: 60)
+                    .background(Color.green)
+                    .cornerRadius(15.0)
             }
-            .navigationBarTitle("Log In")
         }
+        .padding()
     }
-    
-    func logIn() {
-        // Implement the login logic here
-        // This might include form validation and sending data to a server
-        print("Logging in with Name: \(name), Phone: \(phone), Password: \(password)")
+
+    func login() {
+        // Perform login action here
+        // Validate usernameOrPhone and password
+        // You might need to differentiate between a phone number and a username
+        // Then proceed with the login logic
+        print("Login button pressed")
     }
 }
 
@@ -48,3 +51,4 @@ struct LoginPage_Previews: PreviewProvider {
         LoginPage()
     }
 }
+
